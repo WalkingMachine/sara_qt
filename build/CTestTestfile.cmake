4a6442123dd1545cea8ab34b876b573df2f2e21d
Francis Gregoire <gregoire.francis@gmail.com>
2017-02-04 11:54:38 -0500

    This is to fix all

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucas/Sara_QT_work/src
-# Build directory: /home/lucas/Sara_QT_work/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(sara_qt_test1)
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/lucas/Sara_QT_work/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
